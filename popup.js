2934a811f7af7e83709279dcb4e8686e7df85fe4
Jesus Lopez <jlopez521@csu.fullerton.edu>
2025-10-15 21:20:30 -0700

    added a diff pop up:

--- popup.js
+++ popup.js
@@ -1,5 +1,9 @@
     document.getElementById('feedUser').addEventListener('click', () => {
-      alert('MMM YUM!');
+      const msg = document.getElementById('message');
+      msg.style.display = "block";
+      setTimeout(() => {
+        msg.style.display = "none";
+      }, 2000);
     });
     document.getElementById('next').addEventListener('click', () => {
       alert("We don't shower... give it a week.");
