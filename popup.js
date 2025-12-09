c74bd1a8112a364a7c9cd4e8813a7f7351fbfa9b
Jesus Lopez <jlopez521@csu.fullerton.edu>
2025-12-08 20:27:26 -0800

    didn't strat animation

--- popup.js
+++ popup.js
@@ -1,10 +1,12 @@
-    document.getElementById('feedUser').addEventListener('click', () => {
-      const msg = document.getElementById('message');
-      msg.style.display = "block";
-      setTimeout(() => {
-        msg.style.display = "none";
-      }, 2000);
-    });
+document.getElementById('feedUser').addEventListener('click', () => {
+  const msg = document.getElementById('message');
+  msg.style.display = "block";
+  setTimeout(() => {
+    msg.style.display = "none";
+  }, 2000);
+
+  startPizzaAnimation();
+});
     document.getElementById('next').addEventListener('click', () => {
       const msg = document.getElementById('text');
       msg.style.display = "block";
