7a79c569529be1fc9a6b4eb6a0810caf90f8da2a
Jesus Lopez <jlopez521@csu.fullerton.edu>
2025-12-08 20:11:24 -0800

    trying to see if i can animate something

--- popup.js
+++ popup.js
@@ -12,3 +12,15 @@
         msg.style.display = "none";
       }, 2000);
     });
+        // Example for moving a sprite with JavaScript
+    let spriteElement = document.getElementById('mySprite');
+    let xPos = 0;
+
+    function animateSprite() {
+        xPos += 5; // Move 5 pixels to the right
+        spriteElement.style.left = xPos + 'px';
+        requestAnimationFrame(animateSprite);
+    }
+
+    // Call animateSprite() to start the animation
+    // You might trigger this on a button click or when the extension loads
